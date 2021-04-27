589cc270ba5589365a9d4326187d27392c82f1bf
Chiến Nguyễn <chien20x70@gmail.com>
2021-04-27 15:37:24 +0700

    finish Base architectute

--- MISA.Core/Model/CustomerGroup.cs
+++ MISA.Core/Model/CustomerGroup.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MISA.Core.Model
 {
     public class CustomerGroup
     {
         public Guid CustomerGroupId { get; set; }
+
+        [Required(ErrorMessage = "CustomerGroupName khong the de trong!")]
         public string CustomerGroupName { get; set; }
         public string Description { get; set; }
         public DateTime? CreatedDate { get; set; }
